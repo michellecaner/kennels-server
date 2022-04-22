be2774fa95a696c5741369e7b64c8abeca8c3d2e
michellecaner <michellecaner@gmail.com>
2022-04-22 16:14:36 -0500

    sql queries working correctly for locations

--- models/location.py
+++ models/location.py
@@ -1,10 +1,9 @@
 class Location():
     """Creates a new object in the Location class"""
-    def __init__(self, id, name, address, status):
+    def __init__(self, id, name, address):
         self.id = id
         self.name = name
         self.address = address
-        self.status = status
 
-new_location = Location(3, "Central Chattanooga", "333 Choo Choo Court", "Open for Business")
+new_location = Location(3, "Central Chattanooga", "333 Choo Choo Court")
     
